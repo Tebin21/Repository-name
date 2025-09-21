b04de2bcff06604072432230c6a21672b119a979
tebin faruq <tebinfaruq@tebins-MacBook-Pro.local>
2025-09-21 15:52:40 +0300

    Auto-deploy: Update from local changes

--- next.config.js
+++ next.config.js
@@ -6,6 +6,10 @@ const nextConfig = {
   },
   basePath: process.env.NODE_ENV === 'production' ? '/barannnn' : '',
   assetPrefix: process.env.NODE_ENV === 'production' ? '/barannnn/' : '',
+  // Enable static export for GitHub Pages in production
+  ...(process.env.NODE_ENV === 'production' && {
+    output: 'export'
+  })
 }
 
 module.exports = nextConfig
